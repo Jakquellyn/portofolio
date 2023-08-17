0b6aa7bce711f1200c0e8a56fcccc36ff78826d0
Syah <asyaharadea@gmail.com>
2023-08-17 23:43:11 +0700

    upload

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,30 @@
+// links
+import Link from "next/link";
+
+//Icon
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiWhatsappLine,
+  RiPinterestLine, 
+  RiDribbbleLine,
+} from "react-icons/ri";
+
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex items-center gap-x-5 text text-lg">
+    <Link href={''} className='hover:text-accent transition-all duration-300'>
+    <RiInstagramLine/>
+    </Link>
+    <Link href={''} className='hover:text-accent transition-all duration-300'>
+    <RiWhatsappLine />
+    </Link>
+    <Link href={''} className='hover:text-accent transition-all duration-300'>
+    <RiDribbbleLine />
+    </Link>
+   
+  </div>;
 };
 
 export default Socials;
